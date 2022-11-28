e1f753c675e884e33d69d1fd420a2b9b3644c586
Svetlana <111079499+SvetlanaKhaichenko@users.noreply.github.com>
2022-11-28 11:22:09 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,12 +4,7 @@ week = int(input('Введите цифру дня недели: \n'))
 
 if week == 7 or week == 6:
     print('Ура, выходной!')
-elif week > 7:
-    if week%7==0:
-        print('Ура, выходной!')
-    elif week%6==0:
-        print('Ура, выходной!')
-    else:
-        print ('Рабочий день')
-else:
-        print ('Рабочий день')
+elif week < 7 and week > 0:
+    print('Рабочий день')
+elif week <1 or week > 7:
+    print('Введите день недели от 1 до 7.')
